66828e64b85ce8696d049049577ea218a9590cb4
Liam Stevens <me@liamstevens.me>
2022-03-03 14:31:12 +1100

    expand remap

--- src/remap.py
+++ src/remap.py
@@ -7,10 +7,19 @@ class tfResource:
         self.toaddr = toaddr
     
     def generate_moved_block(self):
-        lines = []
-        lines.append("moved {\n")
-        lines.append("from = "+self.fromaddr+"\n")
-        lines.append("to = "+self.toaddr+"\n")
-        lines.append("}\n")
+        self.lines = []
+        self.lines.append("moved {\n")
+        self.lines.append("from = "+self.fromaddr+"\n")
+        self.lines.append("to = "+self.toaddr+"\n")
+        self.lines.append("}\n\n")
 
+def reader(path):
+    hcl_files = []
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            for file in os.listdir(dir):
+                if file.endswith(".tf"):
+                    hcl_files.append(os.path.join(root,name))
+    for name in hcl_files:
+        
 
